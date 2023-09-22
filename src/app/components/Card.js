0b35733bc4ff8011294719d615974d718eb81981
Judge-Paul Ogebe <110723341+Judge-Paul@users.noreply.github.com>
2023-09-22 22:18:52 +0100

    Update Card.js

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -3,7 +3,7 @@ export default function Card({ img, title }) {
     <div className="mt-7 w-[90%] h-80 rounded-lg border-2 border-white text-center">
       <img
         src={img}
-        alt={title + "Image"}
+        alt={"an image of " + title}
         className="h-[260px] rounded-t-lg w-full"
       />
       <h4 className="font-semibold line-clamp-2 my-2">{title}</h4>
