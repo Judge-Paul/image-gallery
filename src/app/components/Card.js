a52203d55e9a630dfb30177326a03a1063e51f6a
Judge-Paul Ogebe <110723341+Judge-Paul@users.noreply.github.com>
2023-09-22 22:25:11 +0100

    Update Card.js

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -1,11 +1,13 @@
 export default function Card({ img, title }) {
   return (
-    <div className="mt-7 w-[90%] h-80 rounded-lg border-2 border-white text-center">
-      <img
-        src={img}
-        alt={"an image of " + title}
-        className="h-[260px] rounded-t-lg w-full"
-      />
+    <div className="mt-7 w-full sm:w-[90%] h-[300px] sm:h-80 rounded-lg border-2 border-white text-center flex flex-col">
+      <div className="flex-grow">
+        <img
+          src={img}
+          alt={"an image of " + title}
+          className="h-[260px] w-full object-cover rounded-t-lg"
+        />
+      </div>
       <h4 className="font-semibold line-clamp-2 my-2">{title}</h4>
     </div>
   );
